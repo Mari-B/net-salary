c21588a0962d5a00b3432601461a11be38634b65
marinabrunello <marina.brunello.91@gmail.com>
2021-06-05 13:01:08 +0100

    table + 2 if statements

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 function calculateTaxes() {
     let grossSalary = document.getElementById("gross-salary").value;
+    let taxDue = document.getElementById("tax-due");
+    let niDue = document.getElementById("NI-due").value;
+    let netPay = document.getElementById("net-pay").value;
+
     if(grossSalary <= 15000) {
-        document.getElementById("demo").innerHTML = "£0"
-}}
+        document.getElementById("tax-due").innerHTML = "0"
+        document.getElementById("NI-due").innerHTML = "0"
+        document.getElementById("net-pay").innerHTML = grossSalary
+    }
+    else if(grossSalary > 15000 && grossSalary <= 50000) {
+        tax = ((grossSalary - 15000)/100) * 20
+        ni = ((grossSalary - 15000)/100) * 12
+        document.getElementById("tax-due").innerHTML = tax
+        document.getElementById("NI-due").innerHTML = ni
+        document.getElementById("net-pay").innerHTML = grossSalary - tax - ni
+    }
+    else {
+        tax = ((grossSalary - 15000)/100) * 20
+        ni = ((grossSalary - 15000)/100) * 12
+        document.getElementById("tax-due").innerHTML = tax
+        document.getElementById("NI-due").innerHTML = ni
+        document.getElementById("net-pay").innerHTML = grossSalary - tax - ni
+    }
+
+}
 
 document.getElementById("button-addon2").addEventListener("click", calculateTaxes);
 
