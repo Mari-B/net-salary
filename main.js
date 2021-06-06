5e8ed69a358399c36888888ba8dff35b40eae0f2
marinabrunello <marina.brunello.91@gmail.com>
2021-06-06 12:38:05 +0100

    logic done +styling

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 function calculateTaxes() {
     let grossSalary = document.getElementById("gross-salary").value;
     let taxDue = document.getElementById("tax-due");
-    let niDue = document.getElementById("NI-due").value;
-    let netPay = document.getElementById("net-pay").value;
+    let niDue = document.getElementById("NI-due");
+    let netPay = document.getElementById("net-pay");
+
+    const formatter = new Intl.NumberFormat('en-UK', {
+        style: 'currency',
+        currency: 'GBP',
+        minimumFractionDigits: 2
+      })
 
     if(grossSalary <= 15000) {
-        document.getElementById("tax-due").innerHTML = "0"
-        document.getElementById("NI-due").innerHTML = "0"
-        document.getElementById("net-pay").innerHTML = grossSalary
+        taxDue.innerHTML = formatter.format(0)
+        niDue.innerHTML = formatter.format(0)
+        netPay.innerHTML = formatter.format(grossSalary)
     }
     else if(grossSalary > 15000 && grossSalary <= 50000) {
         tax = ((grossSalary - 15000)/100) * 20
         ni = ((grossSalary - 15000)/100) * 12
-        document.getElementById("tax-due").innerHTML = tax
-        document.getElementById("NI-due").innerHTML = ni
-        document.getElementById("net-pay").innerHTML = grossSalary - tax - ni
+        taxDue.innerHTML = formatter.format(tax)
+        niDue.innerHTML = formatter.format(ni)
+        netPay.innerHTML = formatter.format(grossSalary - tax - ni)
     }
     else {
-        tax = ((grossSalary - 15000)/100) * 20
-        ni = ((grossSalary - 15000)/100) * 12
-        document.getElementById("tax-due").innerHTML = tax
-        document.getElementById("NI-due").innerHTML = ni
-        document.getElementById("net-pay").innerHTML = grossSalary - tax - ni
+        taxableIncome1 = (grossSalary - 15000) - (grossSalary - 50000)
+        tax1 = (taxableIncome1/100) * 20
+        tax2 = ((grossSalary - 50000)/100) * 40
+        totalTax = tax1 + tax2
+        ni1 = (taxableIncome1/100) * 12
+        ni2 = ((grossSalary - 50000)/100) * 2
+        totalNi = ni1+ni2
+        taxDue.innerHTML = formatter.format(totalTax)
+        niDue.innerHTML = formatter.format(totalNi)
+        netPay.innerHTML = formatter.format(grossSalary - totalTax - totalNi)
     }
 
 }
